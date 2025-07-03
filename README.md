e74743dca34c107a3f9205738960f8453f083d22
shahswati098 <shahswati098@gmail.com>
2025-07-03 15:41:08 +0530

    Clean commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# MERN Stack Project: Real Time Chat App
+## Description
+
+This Real-Time Chat Application is built using the MERN stack (MongoDB, Express, React, Node.js) combined with Socket.io for real-time communication. It allows users to register, authenticate, and engage in instant messaging. The app features real-time updates for online user statuses and leverages JWT for secure authentication and authorization.
+
+## Live Demo
+
+Check out the live demo of the app [![Live Demo](https://img.shields.io/badge/Live-Demo-green.svg)](https://chitchat-ddpw.onrender.com).
+
+
+![Chat App Screenshot](https://github.com/user-attachments/assets/62c80d23-d881-4f5e-8d4e-b6d700ce50c2)
+
+## Features:
+
+- **Tech Stack:** MERN + Socket.io
+- **Authentication & Authorization:** JWT
+- **Real-time Messaging:** Socket.io
+- **Online User Status:** Socket.io and React Context
+- **Global State Management:** Zustand
+- **Error Handling:** Both server and client side
+- **Deployment:** Deployed on Render
+
---
+++ frontend/src/pages/signup/SignUp.jsx
@@ -0,0 +1,107 @@
+import { Link } from "react-router-dom";
+import GenderCheckbox from "./GenderCheckbox";
+import { useState } from "react";
+import useSignup from "../../hooks/useSignup";
+
+const SignUp = () => {
+	const [inputs, setInputs] = useState({
+		fullName: "",
+		username: "",
+		password: "",
+		confirmPassword: "",
+		gender: "",
+	});
+
+	const { loading, signup } = useSignup();
+
+	const handleCheckboxChange = (gender) => {
+		setInputs({ ...inputs, gender });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await signup(inputs);
+	};
+
+	return (
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sl bg-opacity-50'>
+				<h1 className='text-3xl font-bold text-center text-black '>
+					SignUp <span className='text-black font-bold'> ChatApp</span>
+				</h1>
+
+				<form onSubmit={handleSubmit}>
+					<div>
+						<label className='label p-2'>
+							<span className='text-base label-text text-black font-bold'>Full Name</span>
+						</label>
+						<input
+							type='text'
+							placeholder='your name'
+							className='w-full input input-bordered  h-10'
+							value={inputs.fullName}
+							onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+						/>
+					</div>
+
+					<div>
+						<label className='label p-2 '>
+							<span className='text-base label-text text-black font-bold'>Username</span>
+						</label>
+						<input
+							type='text'
+							placeholder='username'
+							className='w-full input input-bordered h-10'
+							value={inputs.username}
+							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+						/>
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text text-black font-bold'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'
+							value={inputs.password}
+							onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+						/>
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text text-black font-bold'>Confirm Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Confirm Password'
+							className='w-full input input-bordered h-10'
+							value={inputs.confirmPassword}
+							onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
+						/>
+					</div>
+
+					<GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
+
+					<Link
+						to={"/login"}
+						className='text-sm hover:underline hover:text-blue-900 mt-2 inline-block font-bold text-black '
+						href='#'
+					>
+						Already have an account?
+					</Link>
+
+					<div>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+							{loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+export default SignUp;
+
